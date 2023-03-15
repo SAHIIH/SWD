49bfaa80e2234748d7ce733cb3851a9c2e2334aa
mchamoudadev <mchamuuda@gmail.com>
2023-03-15 16:56:26 +0300

    added new button to the nav rearanged resume and portfiloi section removed address section integrated with email js to send customer emails smoothly

--- static/js/emailjs.js
+++ static/js/emailjs.js
@@ -9,6 +9,22 @@ window.onload = function () {
         emailjs.sendForm('service_4k7fubb', 'template_yc0jzp9', this)
             .then(function () {
                 console.log('SUCCESS!');
+
+                document.getElementById('contact_form').reset();
+                Toastify({
+                    text: "Thanks for reaching me out, i will contact you soon",
+                    duration: 8000,
+                    destination: "#",
+                    newWindow: true,
+                    close: true,
+                    gravity: "top", // `top` or `bottom`
+                    position: "right", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                        background: "linear-gradient(to right, #619cec, #619cec)",
+                    },
+                    onClick: function () { } // Callback after click
+                }).showToast();
             }, function (error) {
                 console.log('FAILED...', error);
             });
